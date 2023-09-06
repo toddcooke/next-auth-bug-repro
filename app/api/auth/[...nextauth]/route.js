fb043ffb498ebca99125a981e612eefabb258763
Todd Cooke <toddcookevt@gmail.com>
2023-09-06 11:54:55 -0400

    upsert user to db on signin

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,7 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 
 export const handler = NextAuth({
   // Configure one or more authentication providers
@@ -10,6 +12,28 @@ export const handler = NextAuth({
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async signIn({ user }) {
+      const name = user.name;
+      const email = user.email;
+      console.log(email, name, user.email);
+      const upsertUser = await prisma.user.upsert({
+        where: {
+          email: email,
+        },
+        update: {
+          email: email,
+          name: name,
+        },
+        create: {
+          email: email,
+          name: name,
+        },
+      });
+      console.log(upsertUser);
+      return true;
+    },
+  },
 });
 
 export { handler as GET, handler as POST };
--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,3 +1,19 @@
-export default function Private() {
+import { useSession } from 'next-auth/react';
+import { getServerSession } from 'next-auth';
+import { cookies } from 'next/headers';
+import { createClient } from '@supabase/supabase-js';
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { PrismaClient } from '@prisma/client';
+export default async function Private() {
+  // const session = await getServerSession();
+  // console.log(session);
+  // const supabase = createServerComponentClient({ cookies });
+  // const data = await supabase.from('User').select();
+  // console.log(data);
+
+  const prisma = new PrismaClient();
+  const posts = await prisma.user.findMany();
+  console.log(posts);
+
   return <>you should be logged in to see this</>;
 }
